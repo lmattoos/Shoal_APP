62597a8b60d857fb06cf5425957789009b6360af
lmattoos <lorenzobeauvalet.pl014@academico.ifsul.edu.br>
2026-03-25 17:45:02 -0300

    feat: inicio rota entrar, e instalação de dependencias

--- app/entrar.tsx
+++ app/entrar.tsx
@@ -1,11 +1,20 @@
-import { Text, View } from "react-native";
+const requiredMessage = "Campo obrigatório";
+
+const schema = yup
+  .object()
+  .shape({
+    email: yup.string.required(requiredMessage).matches(/\S+@\S+\.\S+/, "Email inválido"),
+    senha: yup.string.required(requiredMessage).matches(
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[$*&@#])[0-9a-zA-Z$*&@#]{8,}$/,
+			"A senha deve conter ao menos uma letra maiúscula, uma letra minúscula, um númeral, um caractere especial e um total de 8 caracteres",
+    ),
+  })
+  .required();
 
 export default function Entrar() {
   return (
     <>
-      <View>
-        <Text></Text>
-      </View>
+      
     </>
   );
 }
