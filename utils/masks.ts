2230cd4b797e5f45d78cd9815306362b89043150
lmattoos <lorenzobeauvalet.pl014@academico.ifsul.edu.br>
2026-04-09 16:54:37 -0300

    feat: screen de signup iniciada, com masks nos inputs necessarios

---
+++ utils/masks.ts
@@ -0,0 +1,26 @@
+export const masks = {
+    cpf: (value: string) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d)/, "$1.$2")
+      .replace(/(\d{3})(\d{1,2})$/, "$1-$2")
+      .substring(0, 14);
+  },
+  cnpj: (value: string) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/^(\d{2})(\d)/, "$1.$2")
+      .replace(/^(\d{2})\.(\d{3})(\d)/, "$1.$2.$3")
+      .replace(/\.(\d{3})(\d)/, ".$1/$2")
+      .replace(/(\d{4})(\d)/, "$1-$2")
+      .substring(0, 18);
+  },
+  telefone: (value: string) => {
+    return value
+      .replace(/\D/g, "")
+      .replace(/(\d{2})(\d)/, "($1) $2")
+      .replace(/(\d{5})(\d)/, "$1-$2")
+      .substring(0, 15);
+  },
+};
