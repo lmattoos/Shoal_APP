bdd7b0937a03af7c6a0181bbcf3cd1ae2092ea52
lmattoos <lorenzo13mattos@gmail.com>
2026-04-15 20:33:06 -0300

    chore: adicionando foto do projeto na Home

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,22 +1,30 @@
-import { StyleSheet, Text } from "react-native";
+import { Image, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
-	return (
-		<SafeAreaView style={styles.container}>
-			<Text style={styles.text}>Home</Text>
-		</SafeAreaView>
-	);
+  return (
+    <SafeAreaView style={styles.container}>
+      <Image
+        style={styles.image}
+        source={require("../../assets/images/shoal/Shoal(Logo).png")}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	text: {
-		fontSize: 50,
-		fontWeight: "bold",
-	},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 50,
+    fontWeight: "bold",
+  },
+  image: {
+    width: 200,
+    height: 200,
+    alignSelf: "center",
+  },
 });
