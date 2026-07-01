8e76d4b5336208d29b92f2337ddd62e28b90fe67
lmattoos <lorenzo13mattos@gmail.com>
2026-07-01 20:18:22 -0300

    feat: estrutura do codigo para manipular notificações iniciado

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,20 @@
 import { AuthContext } from "@/context/AuthProvider";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { router } from "expo-router";
 import { useContext, useEffect } from "react";
-import { Image, StyleSheet, View } from "react-native";
+import { Image, Platform, StyleSheet, View } from "react-native";
 import { useTheme } from "react-native-paper";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function Preload() {
   const theme = useTheme();
@@ -13,18 +25,61 @@ export default function Preload() {
     if (credencial) {
       const resposta = await signIn(credencial);
       if (resposta === "OK") {
-        router.replace("/(tabs)/home");
+        const lastNotification =
+          await Notifications.getLastNotificationResponseAsync();
+        switch (lastNotification?.notification.request.content.data.rota) {
+          case "rota":
+            router.replace("/(tabs)/rota");
+            break;
+          case "rota":
+            router.replace("/(tabs)/rota");
+            break;
+          default:
+            router.replace("/(tabs)/rotaPadrao");
+        }
       } else {
         router.replace("/entrar");
       }
-    } else {
-        router.replace("/entrar");
     }
   }
 
   useEffect(() => {
     logar();
+    registerForPushNotificationsAsync();
   }, []);
+
+  async function registerForPushNotificationsAsync(): Promise<void> {
+    if (Platform.OS === "android") {
+      Notifications.setNotificationChannelAsync("default", {
+        name: "default",
+        importance: Notifications.AndroidImportance.MAX,
+        vibrationPattern: [0, 250, 250, 250],
+        lightColor: theme.colors.error,
+      });
+    }
+
+    if (Device.isDevice) {
+      const { status: existingStatus } =
+        await Notifications.getPermissionsAsync();
+      let finalStatus = existingStatus;
+
+      if (existingStatus !== "granted") {
+        const { status } = await Notifications.requestPermissionsAsync();
+        finalStatus = status;
+      }
+      if (finalStatus !== "granted") {
+        handleRegistrationError(
+          "Permissão negada. Você não receberá notificações até que a permissão seja concedida.",
+        );
+        return;
+      }
+    }
+  }
+
+  function handleRegistrationError(errorMessage: string) {
+    alert(errorMessage);
+    throw new Error(errorMessage);
+  }
 
   return (
     <View
